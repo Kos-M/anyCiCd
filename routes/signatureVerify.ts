ef9467b43dfae3d2baf46d092ffc8629081c2fa7
Kos-M <mpoukouvalas.kk@gmail.com>
2024-03-06 02:56:27 +0200

    make it generic , to work on different git providers
    parse signature sha256 for github
    
    
    fix gitlab too

--- routes/signatureVerify.ts
+++ routes/signatureVerify.ts
@@ -2,9 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import crypto from 'crypto';
 
 const secret: string | undefined = process.env.SECRET;
-const sigHeaderName: string = 'x-gitea-signature';
+const sigHeaderNames: string[] = ['x-gitea-signature', 'X-Gitlab-Token', 'X-Hub-Signature-256'];
 const sigHashAlg: string = 'sha256';
- 
 
 function VerifySignature(req: Request, res: Response, next: NextFunction): void {
   if (!req.body || !req.rawBody) {
@@ -12,23 +11,36 @@ function VerifySignature(req: Request, res: Response, next: NextFunction): void 
     res.status(400).json({ error: 'Request body empty' });
     return;
   }
-  const headerSignature: Buffer = Buffer.from(req.get(sigHeaderName) || '', 'utf8');
   if (!secret) {
     res.status(500).json({ error: 'Internal Server Error', reason: 'Secret is not defined' });
     return;
   }
-
-  const hmac: crypto.Hmac = crypto.createHmac(sigHashAlg, secret);
-  const digest: Buffer = Buffer.from(`${hmac.update(req.rawBody).digest('hex')}`, 'utf8');
-
-  if (headerSignature.length !== digest.length || !crypto.timingSafeEqual(digest, headerSignature)) {
-    res.status(400).json({
-      error: 'Data validation failed',
-      reason: `Request body digest (${digest}) did not match ${sigHeaderName} (${headerSignature})`
-    });
-    return;
-  }
-  next();
+  const rawBody: string = req.rawBody || '';
+  let matchProvider: boolean =false;
+  sigHeaderNames.forEach((headerKey) => {
+    let headerSignature: Buffer = Buffer.from(req.get(headerKey) || '', 'utf8');
+    if (headerKey === 'X-Hub-Signature-256'){
+      const githubSignature = req.get(headerKey)?.replace('sha256=', '');
+      headerSignature = Buffer.from(githubSignature || '', 'utf8');
+    }else if ( headerKey === 'X-Gitlab-Token' && secret != null && secret !== '' && secret !== undefined){ // plain secret format for Gitlab
+      if( secret === req.get(headerKey)  ){
+        matchProvider = true;
+        next();
+      }
+    }
+    const hmac: crypto.Hmac = crypto.createHmac(sigHashAlg, secret);
+    const digest: Buffer = Buffer.from(`${hmac.update(rawBody).digest('hex')}`, 'utf8');
+    
+    if (headerSignature.length == digest.length && crypto.timingSafeEqual(digest, headerSignature)) {
+      matchProvider = true
+      next();
+    }
+  });
+  if ( matchProvider) return;
+  res.status(400).json({
+    error: 'Data validation failed',
+    reason: `Secret validation failed, supported signature headers:${sigHeaderNames}`,
+  });
 }
 
 export { VerifySignature };
